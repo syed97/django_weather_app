dedee93f835b8049f80d7bf15a59d99c0ce3b94c
Syed Hamza Azeem <hamza@marketlytics.com>
2022-04-04 22:24:05 +0500

    changes

--- weather_app/views.py
+++ weather_app/views.py
@@ -3,20 +3,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 import requests
-from .models import City
-from .forms import AddCityForm
+
+from weather_app.constants import WEATHER_API_KEY
+from weather_app.models import City
+from weather_app.forms import AddCityForm
 
 
 def home(request):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=69643db8c08fc089ab6dd8f6a1a9b4cd"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
 
     if request.method == 'POST':
         if 'add_form' in request.POST:
-            form = AddCityForm(data = request.POST)
-            form.save()
-            return HttpResponseRedirect(reverse("home"))
+            data = request.POST
+            if ("," in data["name"]):
+                print("multiple entries found")
+                return HttpResponseRedirect(reverse("home"))
+            else:
+                form = AddCityForm(data)
+                form.save()
+                return HttpResponseRedirect(reverse("home"))
         elif 'del_form' in request.POST:
             city_names_string = request.POST['name']
+            original_to_delete = City.objects.filter(name=city_names_string)
+            if original_to_delete.exists():
+                original_to_delete.delete()
             city_names_list = city_names_string.split(",")
             for city in city_names_list:
                 city_to_delete = City.objects.filter(name=city)
@@ -33,28 +43,33 @@ def home(request):
     weather_data = []
 
     for city in cities:
-        r = requests.get(url.format(city)).json()
-        # data
-        temperature = round(float(r["main"]["temp"]) - 273.15, 2) # in celsius
-        feels_like = round(float(r["main"]["feels_like"]) - 273.15, 2)
-        humidity = round(float(r["main"]["humidity"]),2) # in %
-        wind_speed = r["wind"]["speed"] # in m/s
-        description = r["weather"][0]["description"]
-        icon = r["weather"][0]["icon"]
+        resp = requests.get(url.format(city, WEATHER_API_KEY))
+        if resp.status_code == 200:
+            r = resp.json()
+            # data
+            temperature = round(float(r["main"]["temp"]) - 273.15, 2) # in celsius
+            feels_like = round(float(r["main"]["feels_like"]) - 273.15, 2)
+            humidity = round(float(r["main"]["humidity"]),2) # in %
+            wind_speed = r["wind"]["speed"] # in m/s
+            description = r["weather"][0]["description"]
+            icon = r["weather"][0]["icon"]
 
-        city_weather = {
-            "city": city,
-            "temperature": temperature,
-            "feels_like" : feels_like,
-            "humidity" : humidity,
-            "wind_speed" : wind_speed,
-            "description": description,
-            "icon": icon
-        }
+            city_weather = {
+                "city": city,
+                "temperature": temperature,
+                "feels_like" : feels_like,
+                "humidity" : humidity,
+                "wind_speed" : wind_speed,
+                "description": description,
+                "icon": icon
+            }
 
-        weather_data.append(city_weather)
+            weather_data.append(city_weather)
+        else:
+            continue
 
-    context = {"weather_data" : weather_data, 
-    'add_city_form' : add_city_form
+    context = {
+        "weather_data" : weather_data, 
+        'add_city_form' : add_city_form
     }
     return render(request, 'weather_app/home.html', context)
